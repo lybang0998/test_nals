db05805fd550ffee545d1b665bda5d671a666b70
An Khang <lybang0998@gmail.com>
2021-10-16 00:20:35 +0700

    Export folder build

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     },
     stats: 'errors-only',
     output: {
-        path: path.join(__dirname, 'dist'),
+        path: path.join(__dirname, 'build'),
         filename: '[name].js',
         publicPath: '/'
     },
